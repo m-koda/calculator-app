2915b3ad2715943001268b12830153b611ed1979
m-koda <makotokoda82@gmail.com>
2020-06-16 23:48:04 +0900

    トップページの作成

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,23 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="jumbotron">
+    <h1 class="display-4 text-center">計算問題アプリ</h1>
+    <div class="text-center mt-5"><a class="btn btn-outline-dark btn-lg" href="#" role="button">Start</a></div>
 </div>
 @endsection
